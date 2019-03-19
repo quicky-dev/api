779c05afcd7fc10017a5dcb0eecc3421fc624284
Connor Cahill <connorcahill913@gmail.com>
2019-03-19 13:08:25 -0700

    [Adds Setup]: for setup script generator and creates controller function. [NOTE: Untested]

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,12 +4,29 @@ package controllers
 
 import (
     "net/http"
+    "fmt"
+    "log"
+    "os"
 
     "github.com/labstack/echo"
+    "github.com/quicky-dev/generator/generator"
 )
 
 //HelloWorld is a fake function for testing to make sure our server 
 //is setup and working properly
 func HelloWorld(c echo.Context) error {
     return c.String(http.StatusOK, "Hello World!")
+}
+
+
+//GetGeneric creates the Generic setup script and sends it as response
+func GetGeneric(c echo.Context) error {
+    // generates the generic script and returns the uid
+    uid, err := generator.GenerateGeneric() 
+    if err != nil {
+        log.Fatalln("The following error happened generating script: ", err)
+        os.Exit(1)
+    }
+    fmt.Println(uid) 
+    return c.String(http.StatusOK, "Something did indeed happen")
 }
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
     "github.com/labstack/echo"
     "github.com/quicky-dev/api/controllers"
+    "github.com/quicky-dev/generator/generator"
 )
 
 func main() {
     
+    // call the generator packages Init function to setup the script factory file
+    isFileThere := generator.Init("./scripts", true)
+    _ = isFileThere // always should return true (Ask cenz)
     // sets up new instance of echo 
     e := echo.New()
     
