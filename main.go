23dcf153b1db60874f31a64078ca8ab01ac91803
Connor Cahill <connorcahill913@gmail.com>
2019-03-21 17:46:31 -0700

    [Fixes]: problem with route to test download

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/labstack/echo"
-    "github.com/quicky-dev/api/controllers"
-    "github.com/quicky-dev/generator/generator"
+	"github.com/labstack/echo"
+	"github.com/quicky-dev/api/controllers"
+	"github.com/quicky-dev/generator/generator"
 )
 
 func main() {
-    
-    // call the generator packages Init function to setup the script factory file
-    isFileThere := generator.Init("./scripts", true)
-    _ = isFileThere // always should return true (Ask cenz)
-    // sets up new instance of echo 
-    e := echo.New()
 
-    //WIP: currently logs the uid of file to terminal. Calls generator.GenerateGeneric()
-    e.GET("/api/generic", controllers.GetGeneric).Name="Generic-Script"
+	// call the generator packages Init function to setup the script factory file
+	isFileThere := generator.Init("./scripts", true)
+	_ = isFileThere // always should return true (Ask cenz)
+	// sets up new instance of echo
+	e := echo.New()
 
-    // this route is strictly for testing our scripts
-    e.GET("/", func(c echo.Context) error {
-        return c.HTML(http.StatusOK, `<a href="/generic">Download Now</a>`)
-    })
-    
-    // POST Route: send arr's of software to setup, returns a custom setup script
-    e.POST("/api/custom", controllers.GetCustom).Name="Custom-Script"
+	//WIP: currently logs the uid of file to terminal. Calls generator.GenerateGeneric()
+	e.GET("/api/generic", controllers.GetGeneric).Name = "Generic-Script"
 
-    // api listens on PORT: 3000
-    e.Logger.Fatal(e.Start(":3000"))
+	// this route is strictly for testing our scripts
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `<a href="/api/generic">Download Now</a>`)
+	})
+
+	// POST Route: send arr's of software to setup, returns a custom setup script
+	e.POST("/api/custom", controllers.GetCustom).Name = "Custom-Script"
+
+	// api listens on PORT: 3000
+	e.Logger.Fatal(e.Start(":3000"))
 }
